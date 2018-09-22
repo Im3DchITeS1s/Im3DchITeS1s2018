ce4d5924aa225546a6651e0b2f7baef8a36d78c7
RenHec <emisor.tarea@gmail.com>
2018-09-22 13:01:13 -0600

    es necesario

--- app/Seccion.php
+++ app/Seccion.php
@@ -14,11 +14,7 @@ class Seccion extends Model
 		return Seccion::join('estado', 'seccion.fkestado', '=', 'estado.id')
                     ->select(['seccion.id as id', 'seccion.letra as letra', 'seccion.fkestado as id_estado']);
     }
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 75fbd3b1049611b171e246e26b9fd941634fe774
     public static function buscarSeccion($id){
 		return Seccion::select('id', 'letra')
             ->where('fkestado', $id)
