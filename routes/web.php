f14abb46b551c72973cf26e148c6f5294cd6d2ae
RenHec <emisor.tarea@gmail.com>
2018-10-06 15:58:20 -0600

    datatable cargar contenido educativo
    proceso para visualizar el contenido educativo cargado por el catedratico.

--- routes/web.php
+++ routes/web.php
@@ -183,6 +183,7 @@ Route::get('/resolver/cuestionario/seleccionado/blackboard/{id1}', 'ResponderBan
 
 //CargarContenidoCatedratico
 Route::resource('/plataforma/blackboard/cargar/contenido_educativo/catedratico', 'CatedraticoContenidoEducativoController');
+Route::get('cargar/contenido_educativo/catedratico/getdata', 'CatedraticoContenidoEducativoController@getdata')->name('contenido_educativo_catedratico.getdata');
 Route::get('/plataforma/blackboard/cargar/dropInformacionCatedratico', 'CatedraticoContenidoEducativoController@dropInformacionCatedratico');
 Route::get('/plataforma/blackboard/cargar/dropFormatoDocumento', 'CatedraticoContenidoEducativoController@dropFormato');
 
