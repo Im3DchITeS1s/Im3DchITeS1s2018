246ac0211665d9e1801d314cf710edc7bf2b12ba
José Carlos Montepeque Hernández <32318575+carlosmnhz@users.noreply.github.com>
2018-11-03 16:50:03 -0600

    Cuestionario Controller
    Agregue el controlador y las demas cosas

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -306,8 +306,6 @@ class DocenteController extends Controller
 
         return strtoupper($incial.'-'.$correlativo);
     }
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 259712ffc26cef6f1cf939f377abe4f2f1743714
+
--- resources/views/academico/catedraticocurso/catedraticocurso.blade.php
+++ resources/views/academico/catedraticocurso/catedraticocurso.blade.php
@@ -293,18 +293,8 @@
                     $('#fkcarrera_curso_add').val('').trigger('change.select2'); 
                 }
             });
-<<<<<<< HEAD
 
-         function curso(id){
-            if(id.value=fkcantidad_alumno)
-            {
-                $($fkcarrera_curso).data(curso);
-            }
-                            }
-       });        
-
-=======
->>>>>>> parent of 5e1b22f... cambio
+       });       
         
 
         $('.modal-footer').on('click', '.add', function() {
