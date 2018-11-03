4d6a070665eacac9ce37ec87735fe270041c532c
Carlosl2 <carlosmorales_lopez@hotmail.com>
2018-11-03 15:00:24 -0600

    Actualizacion
    Actualizacion

--- app/Pago.php
+++ app/Pago.php
@@ -7,17 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Pago extends Model
 {
     protected $table = 'pago';
-	protected $guarded = ['id', 'fkestado'];
-	protected $fillable = ['monto','fecha'];
+	protected $guarded = ['id', 'fkestado','fkmes','fkinscripcion','fkpersona'];
+	protected $fillable = ['pago'];
 
-	/*public static function dataPago(){
-		return Producto::join('tipo_pago', 'pago.fktipo_pago', '=', 'tipo_pago.id')
-					->join('fkmes', 'pago.fkmes', '=', 'mes.id')
+	public static function dataPago(){
+		return Pago::join('tipo_pago', 'pago.fktipo_pago', '=', 'tipo_pago.id')
+					->join('mes', 'pago.fkmes', '=', 'mes.id')
+					->join('inscripcion', 'pago.fkinscripcion', '=', 'inscripcion.id')
+					->join('persona', 'inscripcion.fkpersona', '=', 'persona.id')
 					->join('estado', 'pago.fkestado', '=', 'estado.id')
-                    ->select(['pago.id as id', 'pago.monto as monto', 'mes.nombre as mes','tipo_pago.nombre as tipo_pago', 'tipo_pago.fkestado as fkestado', 'tipo_pago.fkcategoria as fkcategoria'])
-                    ->orderBy('tipo_pago.id','asc');
+
+                    ->select(['pago.id as id_pago', 'pago.pago as pago', 'mes.nombre as mes','tipo_pago.nombre as tipo_pago', 'tipo_pago.fkestado as fkestado','persona.id as codigo','persona.nombre1 as nombre1','persona.nombre2 as nombre2','persona.apellido1 as apellido1','persona.apellido2 as apellido2']   
     }
 
-	*/
 
+	
+    
 }
