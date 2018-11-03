4f14da2a3b2519a72a6b50be30b42230d5810f3c
RenHec <emisor.tarea@gmail.com>
2018-11-03 15:28:17 -0600

    Revert "historicos"
    This reverts commit 0b864947c31953b9c4815c5f39538a1048888851.

--- routes/web.php
+++ routes/web.php
@@ -256,16 +256,22 @@ Route::resource('/gestionadministrativa/controlpago/tipopago', 'TipoPagoControll
 Route::get('TipoPago/getdata', 'TipoPagoController@getdata')->name('tipopago.getdata');
 Route::post('/gestionadministrativa/controlpago/tipopago/cambiarEstado', 'TipoPagoController@cambiarEstado');
 
+<<<<<<< HEAD
+
 // Pago  
 
 Route::resource('/gestionadministrativa/controlpago/pago', 'PagoController');
 Route::get('Pago/getdata', 'PagoController@getdata')->name('pago.getdata');
 //Route::post('/gestionadministrativa/controlpago/tipopago/cambiarEstado', 'TipoPagoController@cambiarEstado');
 
+
+
 // Cuestionarios Historicos Alumno
 Route::get('/plataforma/blackboard/cuestionario/historicos/alumnohistorico', 'CuestionarioHistoricoAlumno@index')->name('alumnohistorico.index');
 Route::get('get/historicos/alumnohistorico/{carrera}/{curso}/{anio}', 'CuestionarioHistoricoAlumno@getdata')->name('alumnohistorico.getdata');
 Route::get('/filtrar/curso/carrera/{id}', 'CuestionarioHistoricoAlumno@dropCurso');
+=======
+>>>>>>> 722fdd02cae9a582fafe55aadb5b7e369426b03e
 
 Route::get('image-view','ImageController@index');
 Route::post('image-view','ImageController@store');
