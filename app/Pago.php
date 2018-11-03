66abe5ebf5c02f189ee53f67779c70f186114ac4
Carlosl2 <carlosmorales_lopez@hotmail.com>
2018-11-03 11:02:55 -0600

    Actualizar
    Actualizar

--- app/Pago.php
+++ app/Pago.php
@@ -10,4 +10,14 @@ class Pago extends Model
 	protected $guarded = ['id', 'fkestado'];
 	protected $fillable = ['monto','fecha'];
 
+	/*public static function dataPago(){
+		return Producto::join('tipo_pago', 'pago.fktipo_pago', '=', 'tipo_pago.id')
+					->join('fkmes', 'pago.fkmes', '=', 'mes.id')
+					->join('estado', 'pago.fkestado', '=', 'estado.id')
+                    ->select(['pago.id as id', 'pago.monto as monto', 'mes.nombre as mes','tipo_pago.nombre as tipo_pago', 'tipo_pago.fkestado as fkestado', 'tipo_pago.fkcategoria as fkcategoria'])
+                    ->orderBy('tipo_pago.id','asc');
+    }
+
+*/
+
 }
